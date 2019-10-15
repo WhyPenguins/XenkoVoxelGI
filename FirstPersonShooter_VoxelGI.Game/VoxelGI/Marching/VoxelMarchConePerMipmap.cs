b05c441c715ef49e777778544642edc668acc803
WhyPenguins <sean@whypenguins.com>
2019-10-15 18:26:10 +1100

    Add mipmap exact marcher

---
+++ FirstPersonShooter_VoxelGI.Game/VoxelGI/Marching/VoxelMarchConePerMipmap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xenko.Core;
+using Xenko.Shaders;
+
+namespace Xenko.Rendering.Voxels
+{
+    [DataContract(DefaultMemberMode = DataMemberMode.Default)]
+    [Display("Cone (Mipmap Exact)")]
+    public class VoxelMarchConePerMipmap : IVoxelMarchMethod
+    {
+        public int Steps = 7;
+
+        public VoxelMarchConePerMipmap()
+        {
+
+        }
+        public VoxelMarchConePerMipmap(int steps)
+        {
+            Steps = steps;
+        }
+        public ShaderSource GetMarcher(int attrID)
+        {
+            var mixin = new ShaderMixinSource();
+            mixin.Mixins.Add(new ShaderClassSource("VoxelMarchConePerMipmap", Steps));
+            mixin.Macros.Add(new ShaderMacro("AttributeID", attrID));
+            return mixin;
+        }
+        public void UpdateSamplerLayout(string compositionName)
+        {
+        }
+        public void ApplyViewParameters(ParameterCollection parameters)
+        {
+        }
+    }
+}
